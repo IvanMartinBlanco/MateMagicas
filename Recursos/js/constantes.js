ea50e6ad26a143f6af787af2e61e7992c8099bc0
Ivan <ivanmartinblanco1985@gmail.com>
2023-02-26 14:13:22 +0100

    Primer Envio

---
+++ Recursos/js/constantes.js
@@ -0,0 +1,16 @@
+// Sección de constantes compartidas por todas las páginas
+const NOMBRE_SITIO = "Mi Sitio Web";
+const DERECHOS_RESERVADOS = "© 2023 Todos los derechos reservados.";
+const POLITICA_PRIVACIDAD = "Política de Privacidad";
+const TERMINOS_CONDICIONES = "Términos y Condiciones";
+
+// Sección de código para la página "index"
+if (document.title === "Inicio") {
+  // Código específico para la página de inicio
+  const TITULO_PAGINA = "Página de Inicio";
+  const DESCRIPCION_PAGINA = "Bienvenido a la página de inicio";
+}
+
+// Sección de código para la página "informacion"
+if (document.title === "Ejercicios") {
+}
