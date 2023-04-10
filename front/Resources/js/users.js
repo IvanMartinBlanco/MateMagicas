cbb780122a06f2be930a1083896e7e1178729606
Ivan <ivanmartinblanco1985@gmail.com>
2023-04-10 16:05:51 +0200

    Imagenes perfil

--- front/Resources/js/users.js
+++ front/Resources/js/users.js
@@ -2,15 +2,14 @@ import { getSessionData } from '../js/session.js';
 
 // Llamar a la función de sesión al cargar la página
 window.addEventListener('load', getSessionData);
-console.log(getSessionData());
+console.log(getSessionData().registeredUser);
 
 //Carga solamente cuando ya se haya creado el DOM.
 document.addEventListener("DOMContentLoaded", function (event) {
 
-
-	//const imageUrl = user.image && registeredUser ? user.image : "../../../front/Resources/Images/avatar.png";
+	const imageUrl = getSessionData().registeredUser ? `../../../front/Resources/Images/${getSessionData().rol}.png` : "../../../front/Resources/Images/avatar.png";
 	const myImage = document.getElementById("avatar");
-	//myImage.src=imageUrl;
+	myImage.src=imageUrl;
 	//Función que define el texto que se muestra en el acceso
 
 		  if (getSessionData()?.registeredUser) {
