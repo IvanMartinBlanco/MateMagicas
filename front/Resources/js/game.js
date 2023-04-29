bdaf74d6c79434442a10330277f578115ff91c12
Ivan <ivanmartinblanco1985@gmail.com>
2023-04-29 15:07:28 +0200

    Eliminar target erroneo de varios links

--- front/Resources/js/game.js
+++ front/Resources/js/game.js
@@ -5,6 +5,36 @@ document.addEventListener("DOMContentLoaded", function () {
         // Redirigir a otra página
         window.location.replace("../../../front/pages/login.html");
       }
-    
+      const queryString = window.location.search;
+
+      // Analizar la cadena de consulta para obtener los parámetros
+      const params = new URLSearchParams(queryString);
+      const stage = params.get('stage');
+      const subject = params.get('subject');
+    console.log(stage)
+    console.log(subject)
+  // Llamada al primer fetch
+  fetch(`http://localhost/web/back/public/getworksbysubject?stage=${stage}&subject=${subject}`)
+    .then(response => {
+      if (!response.ok) {
+        serverMessage.textContent = "Error en la respuesta del servidor";
+      }
+      return response.json();
+    })
+    .then(data => {
+      const sortedData = Object.entries(data)
+        .sort((a, b) => a[0] - b[0])
+        .map(entry => entry[1]);
+      variableInputs.forEach((input, index) => {
+        const inputValue = sortedData[index] || "";
+        input.value = inputValue;
+
+        // Guardar el ID de la variable correspondiente en el array
+        variableIds.push(Object.keys(data)[index]);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
 
 });
